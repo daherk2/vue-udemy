16995fac0f2f52e8f8f06e16bf5bc3f69f4b5eeb
Igor Octaviano <igoroctaviano@gmail.com>
2018-11-06 15:00:19 -0200

    Add header to first module project

---
+++ projects/firstModulesProject/src/components/Quote.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="panel panel-default">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  .panel-body {
+    font-size: 24px;
+  }
+
+  .quote {
+    cursor: pointer;
+  }
+
+  .quote:hover {
+    background-color: red;
+  }
+</style>
+
---
+++ projects/firstModulesProject/src/components/QuoteGrid.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <app-quote v-for="(quote, i) in quotes" @click.native="deleteQuote(i)">
+      {{ quote }}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue';
+
+  export default {
+    components: {
+      appQuote: Quote
+    },
+    props: ['quotes'],
+    methods: {
+      deleteQuote(i) {
+        this.$emit('deleteQuote', i);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
